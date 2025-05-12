2e4b3dc55415f8eafbb39edf9d4f66c4fea6ce8e
Faithson-DOSSOU <faithsondossou1@gmail.com>
2025-05-12 11:54:21 +0200

    Work in progress

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,11 +1,26 @@
 <script>
+import event from '../assets/png/events_bg.png';
 
 export default {
   name : 'Events',
+  data() {
+    return {
+      event
+    }
+  }
 };
 </script>
 
 <template>
+  <div class="event-page">
+    <div></div>
+    <p class="page-title">Evènements</p>
+    <div class="event-slider">
+      <div class="slider-wrapper">
+        <img class="event-cover" :src="event" alt="">
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -13,5 +28,28 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+.event-page{
+  width: 100%;
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.page-title{
+  font-size: 25px;
+}
+.event-slider{
+  width: 1000px;
+  height: 500px;
+}
+.slider-wrapper{
+  width: 100%;
+  height: 100%;
+  border-radius: 50px;
+  overflow: hidden;
+}
+.event-cover{
+  width: 100%;
 }
 </style>
